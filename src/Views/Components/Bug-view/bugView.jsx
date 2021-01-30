77325c2f5b9596797d774f532eecfd46a7478410
Serhii <malgothsm@gmail.com>
2021-01-30 22:19:46 +0200

    Almost finished view bugs window

--- src/Views/Components/Bug-view/bugView.jsx
+++ src/Views/Components/Bug-view/bugView.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ViewSection from './bugViewSection';
 import BugModel from "../../../Models/bugModel";
+import {useDispatch} from 'react-redux';
+import {markComplete} from '../../../Controllers/Redux/bugSlice';
 import './bugView.css';
 
 export default (props) => {
+  const dispatch = useDispatch();
   const bug = new BugModel(props.bug);
   return (
     <div className="bug-view">
-      <h2>{bug.name}</h2>
-      <ViewSection title="Details" info={bug.detail} />
+      <button onClick={props.clicked} className="close-Btn">Close</button>
+
+      <h1>{bug.name}</h1>
+      <ViewSection title="Details" info={bug.details} />
       <ViewSection title="Steps" info={bug.steps} />
       <ViewSection title="Priority" info={bug.priority} />
       <ViewSection title="Creator" info={bug.creator} />
       <ViewSection title="App version" info={bug.version} />
       <ViewSection title="Time created" info={bug.time} />
+      <button onClick={()=>{dispatch(markComplete())}} className="">Mark as a completed</button>
     </div>
   );
 };
