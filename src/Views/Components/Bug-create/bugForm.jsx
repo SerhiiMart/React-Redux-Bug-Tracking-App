8a97aca3f3a96211960ebd9dc7acf5ce215f1a25
Serhii <malgothsm@gmail.com>
2021-01-31 11:57:09 +0200

    Working on form for bugs

--- src/Views/Components/Bug-create/bugForm.jsx
+++ src/Views/Components/Bug-create/bugForm.jsx
@@ -9,7 +9,22 @@ export default (props) => {
         <label htmlFor="">Name:</label>
         <input name="name" placeholder='Bug Name' type="text" required> </input>
         <label htmlFor="">Details:</label>
-        <textarea name="details" placeholder='Detailed desctiption on the bug' requred></textarea>
+        <textarea name="details" placeholder='Detailed desctiption of the bug' requred></textarea>
+        <label htmlFor="">Steps:</label>
+        <textarea name="steps" placeholder='Steps to reacreate the bug' requred></textarea>
+        <label htmlFor="">Priority</label>
+        <select name="priority" requred>
+            <option value="1">High</option>
+            <option value="2">Medium</option>
+            <option value="3">Low</option>
+        </select>
+        <label htmlFor="">Assigned</label>
+        <select name="assigned" >
+            <option>SerhiiMart</option>
+        </select>
+        <label htmlFor="">Aplication Version:</label>
+        <input name="version" placeholder="aplication version"></input>
+        <button type="submit">{props.title}</button>
       </form>
     </div>
   )
